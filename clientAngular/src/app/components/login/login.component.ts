523e680cb4054aafd029412518bcc021c3e6e867
mihaitir <mihai.tir98@e-uvt.ro>
2019-11-22 00:40:46 +0200

    add teacher component

--- clientAngular/src/app/components/login/login.component.ts
+++ clientAngular/src/app/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { LoginService } from './login.service'
 import { HttpClient } from '@angular/common/http';
 import { Account } from '../models/account';
+import { Teacher } from '../models/teacher';
 
 @Component({
   selector: 'app-login',
@@ -27,17 +28,20 @@ invalidAccount : boolean = false;
   onSubmit(s:string){
    let usern = this.loginForm.controls['username'].value;
    let passs = this.loginForm.controls['password'].value;
-   this.loginService.onlogin(usern,passs).subscribe( res=>
+   this.loginService.onlogin(usern,passs).subscribe( (res:Account)=>
     {
-      console.log(res);
-      localStorage.setItem('isLoggedIn', "true");
-      this.router.navigate(['teacher/myClassooms']);  
+      //here we check if the user is a teacher or a student...
+      if(res.isTeacher){ 
       this.loginService.getTeacherByUsername(usern).subscribe(
-        (res)=>{
-          console.log(res);
-          localStorage.setItem('teacherId','1')}
-      )
-     
+        (res:Teacher)=>{
+          //we use the id of teacher from database
+          localStorage.setItem('teacherId',res.idTeacher.toString())
+          localStorage.setItem('isLoggedIn', "true");
+          this.router.navigate(['teacher/myClassooms']); })
+      }
+      else{
+        console.log('we must implement in the next increment get user')
+      } 
     },
     err=>{
       if (err.status === 401) this.invalidAccount = true});
